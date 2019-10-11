df415b83b925ada7343be9a2ba7edd720556a2f4
Kaelib <kaelibw84@gmail.com>
2019-10-11 15:50:41 -0500

    Finished code using bracket notation to invoke methods creating a simple calculator

--- scripts.js
+++ scripts.js
@@ -2,16 +2,16 @@
 const calculator = {
   add(a, b) {
     return Number(a) + Number(b);
-  }
+  },
   subtract(a, b) {
     return a - b;
-  }
+  },
   multiply(a, b) {
     return a * b;
-  }
+  },
   divide(a, b) {
     return a / b;
-  }
+  },
   remainder(a, b) {
     return a % b;
   }
@@ -21,7 +21,10 @@ const form = document.querySelector("form");
 const num1 = document.querySelector("#num-1");
 const num2 = document.querySelector("#num-2");
 const select = document.querySelector("select");
+const output = document.querySelector("output");
 
 form.addEventListener("submit", function event){
   event.preventDefault();
+  console.log(num1.value,num2.value, select.value);
+  output.textContent = calculator[select.value](num1.value, num2.value);
 }
